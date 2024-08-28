e470402e99ed9cd8790b2a9b59c46f98b66fc349
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-08-28 11:42:09 +0200

    inital commit

---
+++ home_manager/.config/wofi-power.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+entries=" Logout\n Suspend\n󰑓 Reboot\n󰐥 Shutdown"
+
+selected=$(echo -e $entries|wofi --width 250 --height 100 --dmenu --cache-file /dev/null --xoffset 2310 --yoffset 0 | awk '{print tolower($2)}')
+
+case $selected in
+  logout)
+    swaymsg exit;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
