fbabb85469cf1bd51b2f9a4ab5a506c601f0a07f
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2025-01-30 23:57:32 +0100

    changed some stuff

--- install-endeavour.sh
+++ install-endeavour.sh
@@ -3,12 +3,12 @@
 # This script installs rebos and sets it up.
 
 sudo echo -e "\n[oglo-arch-repo]\nSigLevel = Optional DatabaseOptional\nServer = https://gitlab.com/Oglo12/\$repo/-/raw/main/\$arch" | sudo tee -a /etc/pacman.conf
-sudo pacman -Syy
 
+yay -Syy
 yay -S rebos --noconfirm
 
 rebos setup
 rebos config init
 rebos gen commit "inital"
 rebos gen current build
-rebos gen current sync
+rebos managers sync
